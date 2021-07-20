532c27725b03b5ac69dd26465d4ff22489402fae
Edikan Bassey <60517569+Mestareddy@users.noreply.github.com>
2021-07-20 18:58:11 +0100

    Update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,20 +14,34 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 const posts = [];
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongoose.model('Post', postSchema)
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('./pages/home',{
-    title: 'Home', 
-    content: homeStartingContent, 
-    newPost: posts})
+  Post.find({}, (err, posts) => {
+    res.render('./pages/home', {
+      content: homeStartingContent,
+      newPost: posts
+    })
+  })
 })
 
 app.get('/contact', (req, res) => {
-  res.render('./pages/contact', {title: 'Contact', content: contactContent})
+  res.render('./pages/contact', { title: 'Contact', content: contactContent })
 })
 
 app.get('/about', (req, res) => {
@@ -38,30 +53,38 @@ app.get('/compose', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
-
-  const post = {
+  const post = new Post({
     title: req.body.newTitle,
     content: req.body.newContent
-  }
+  })
 
-  posts.push(post)
-  res.redirect('/')
-})
-
-
-app.get('/pages/:topic', (req, res) => {
-  const postLink = _.kebabCase(req.params.topic);
-
-  posts.forEach((post) => {
-    const calledTitle = _.kebabCase(post.title);
-
-    if (calledTitle === postLink ) {
-      res.render('./pages/post', { title:post.title, content:post.content })
+  post.save((err) => {
+    if (!err) {
+      res.redirect('/')
     }
   })
 })
 
 
-app.listen(port, function() {
-  console.log("Server started on port" + port);
+app.get('/pages/:topic', (req, res) => {
+  const postLink = _.capitalize(req.params.topic);
+
+  Post.findOne({_id: postLink}, (err, post)=> {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    })
+  })
+  // posts.forEach((post) => {
+  //   const calledTitle = _.kebabCase(post.title);
+
+  //   if (calledTitle === postLink) {
+  //     res.render('./pages/post', { title: post.title, content: post.content })
+  //   }
+  // })
+})
+
+
+app.listen(port, function () {
+  console.log("Server started on port " + port);
 });
